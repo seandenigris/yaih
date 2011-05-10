527b80bb478fb9805809036418db2ef90c328abf
lucapette <lucapette@gmail.com>
2011-05-11 00:19:00 +0200

    correct indentation

--- lib/yaih/core.rb
+++ lib/yaih/core.rb
@@ -1,20 +1,20 @@
 module Yaih
-    module Core # This module contains methods for interfacing with IRB history
+  module Core # This module contains methods for interfacing with IRB history
 
-        extend self
+    extend self
 
-        # Returns an array of IRB history entries
-        # bases of the given n.
-        def entries(n=Readline::HISTORY.size)
-            size=Readline::HISTORY.size
-            Readline::HISTORY.to_a[(size - n)..size-1]
-        end
-
-        # Returns an array of strings
-        def decorate(n)
-            size=Readline::HISTORY.size
-            ((size - n)..size-1).zip(entries(n)).map {|e| e.join(" ")}
-        end
-
+    # Returns an array of IRB history entries
+    # bases of the given n.
+    def entries(n=Readline::HISTORY.size)
+      size=Readline::HISTORY.size
+      Readline::HISTORY.to_a[(size - n)..size-1]
     end
+
+    # Returns an array of strings
+    def decorate(n)
+      size=Readline::HISTORY.size
+      ((size - n)..size-1).zip(entries(n)).map {|e| e.join(" ")}
+    end
+
+  end
 end
--- lib/yaih/kernel.rb
+++ lib/yaih/kernel.rb
@@ -1,38 +1,38 @@
 module Kernel # Adding methods to Kernel module makes possible to use them in irb.
 
-    # Prints the latest n lines from IRB
-    # history. It decorates each line with
-    # the correspondent command number.
-    # @return [Fixnum] number of printed lines.
-    def h(n=10)
-        entries = Yaih::Core.decorate(n)
-        puts entries
-        entries.size
-    end
+  # Prints the latest n lines from IRB
+  # history. It decorates each line with
+  # the correspondent command number.
+  # @return [Fixnum] number of printed lines.
+  def h(n=10)
+    entries = Yaih::Core.decorate(n)
+    puts entries
+    entries.size
+  end
 
-    # It greps history and prints matched lines.
-    # @return [Fixnum] number of printed lines.
-    # @todo Consider to highlight matched part.
-    def hgrep(word)
-        matched=Yaih::Core.decorate(Readline::HISTORY.size - 1).select {|h| h.match(word)}
-        puts matched
-        matched.size
-    end
+  # It greps history and prints matched lines.
+  # @return [Fixnum] number of printed lines.
+  # @todo Consider to highlight matched part.
+  def hgrep(word)
+    matched=Yaih::Core.decorate(Readline::HISTORY.size - 1).select {|h| h.match(word)}
+    puts matched
+    matched.size
+  end
 
-    # It takes the range of lines to execute,
-    # evals the lines and prints them.
-    # @return [nil]
-    def h!(start, stop=nil)
-        stop=start unless stop
-        lines = Yaih::Core.entries[start..stop]
-        lines.each_with_index { |e,i|
-            irb_context.evaluate(e,i)
-        }
-        Readline::HISTORY.pop
-        lines.each { |l|
-            Readline::HISTORY.push l
-        }
-        puts lines
-    end
+  # It takes the range of lines to execute,
+  # evals the lines and prints them.
+  # @return [nil]
+  def h!(start, stop=nil)
+    stop=start unless stop
+    lines = Yaih::Core.entries[start..stop]
+    lines.each_with_index { |e,i|
+      irb_context.evaluate(e,i)
+    }
+    Readline::HISTORY.pop
+    lines.each { |l|
+      Readline::HISTORY.push l
+    }
+    puts lines
+  end
 
 end
